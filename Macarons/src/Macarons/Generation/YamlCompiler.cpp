725253459054de4e2eeb5cc3a44019ec9b44777f
AlexandruIstrate <38143225+AlexandruIstrate@users.noreply.github.com>
2020-07-04 23:16:51 +0300

    Added YAML version reading

--- Macarons/src/Macarons/Generation/YamlCompiler.cpp
+++ Macarons/src/Macarons/Generation/YamlCompiler.cpp
@@ -10,55 +10,6 @@ namespace Macarons
 {
 	using namespace YAML;
 
-	class MacaronsYamlEventHandler : public EventHandler
-	{
-	public:
-		void OnDocumentStart(const Mark& mark) override
-		{
-			throw std::logic_error("The method or operation is not implemented.");
-		}
-
-		void OnDocumentEnd() override
-		{
-			throw std::logic_error("The method or operation is not implemented.");
-		}
-
-		void OnNull(const Mark& mark, anchor_t anchor) override
-		{
-			throw std::logic_error("The method or operation is not implemented.");
-		}
-
-		void OnAlias(const Mark& mark, anchor_t anchor) override
-		{
-			throw std::logic_error("The method or operation is not implemented.");
-		}
-
-		void OnScalar(const Mark& mark, const std::string& tag, anchor_t anchor, const std::string& value) override
-		{
-			throw std::logic_error("The method or operation is not implemented.");
-		}
-
-		void OnSequenceStart(const Mark& mark, const std::string& tag, anchor_t anchor, EmitterStyle::value style) override
-		{
-			throw std::logic_error("The method or operation is not implemented.");
-		}
-
-		void OnSequenceEnd() override
-		{
-			throw std::logic_error("The method or operation is not implemented.");
-		}
-
-		void OnMapStart(const Mark& mark, const std::string& tag, anchor_t anchor, EmitterStyle::value style) override
-		{
-			throw std::logic_error("The method or operation is not implemented.");
-		}
-
-		void OnMapEnd() override
-		{
-			throw std::logic_error("The method or operation is not implemented.");
-		}
-	};
-
 	YamlCompiler::YamlCompiler(const std::string& configPath) : m_YamlFilePath{ configPath }
 	{
 		Initialize();
@@ -70,17 +21,17 @@ namespace Macarons
 
 	SemanticVersion YamlCompiler::GetVersion() const
 	{
-		// Reload the file path
-		m_Parser->Load(std::ifstream(m_YamlFilePath));
+		const Node& versionNode = (*m_RootNode)["version"];
 
-		m_Parser->HandleNextDocument(*m_EventHandler);
+		const Node& major = versionNode["major"];
+		const Node& minor = versionNode["minor"];
+		const Node& patch = versionNode["patch"];
 
-		return SemanticVersion(1, 0, 0);
+		return SemanticVersion(major.as<int>(), minor.as<int>(), patch.as<int>());
 	}
 
 	void YamlCompiler::Initialize()
 	{
-		m_Parser = std::make_unique<Parser>(std::ifstream(m_YamlFilePath));
-		m_EventHandler = std::make_unique<MacaronsYamlEventHandler>();
+		m_RootNode = std::make_unique<Node>(YAML::LoadFile(m_YamlFilePath));
 	}
 }
--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -8,24 +8,6 @@ int main()
 	Log::Init();
 
 	Repository repo("Path here");
-	
-	for (const Branch& branch : repo.GetBranches(BranchType::Remote))
-	{
-		auto upstream = branch.GetUpstream();
-		const std::string& name = upstream.has_value() ? upstream->GetDisplayName() : "Not Available";
-
-		MR_INFO("---------- Branch {0}; Active {1}; Upstream {2} ----------\n", branch.GetName(), branch.IsActive(), name);
-
-		for (const Commit& commit : branch.GetCommits())
-		{
-			//MR_INFO("Commit[{0}] - {1}", commit.GetCommitDate(), commit.GetMessage());
-
-			for (const Tag& tag : commit.GetTags())
-			{
-				MR_INFO("Tag {0}; {1} ({2}, {3})", tag.GetName(), tag.GetMessage(), tag.GetTagger().Name, tag.GetTagger().Email);
-			}
-		}
-	}
 
 	YamlCompiler comp("Path here");
 	SemanticVersion version = comp.GetVersion();
